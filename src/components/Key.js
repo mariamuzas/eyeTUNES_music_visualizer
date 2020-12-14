a72b51ef8d0afb30efbc85dc5544069f97858f77
AvianLearning <iaradcliffe@hotmail.com>
2020-12-14 15:46:54 +0000

    Working on colour change for keys...

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,11 +1,43 @@
 import * as Tone from 'tone';
+import styled from 'styled-components';
+import { useState } from 'react';
 
+let Pad = styled.li.attrs(props => ({
+    style: {
+        backgroundColor: props.color
+    }
+}))` 
+    height: 20vh;
+    width: 20vw;
+    margin: 5px;`
 
 const Key = ({individualPad, onKeyClick}) => {
 
+    const [currentColour, setCurrentColour] = useState(false);
+
+    // When we click 'a' on keyboard - we want note to sound, and background colour to change
+
+    const whichColour = () => {
+        if (currentColour) {
+            Pad = styled.li.attrs(props => ({
+                style: {
+                    backgroundColor: "black"
+                }
+            })) 
+            
+        }
+        else {
+            Pad = styled.li.attrs(props => ({
+                style: {
+                    backgroundColor: props.color
+                }
+            }))
+    }
+}
+
      return (
          <>
-            <li>{individualPad.keyPress}</li>
+            <Pad color={individualPad.color} onKeyClick={() => whichColour()}>{individualPad.keyPress}</Pad>
          </>
      )
 }
