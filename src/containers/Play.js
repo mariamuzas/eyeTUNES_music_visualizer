bbb6132925336b23153ef473c9f8d0d5e3c92fae
Your Name <mmuzasantuna@gmail.com>
2020-12-14 20:52:35 +0000

    can save song to a playlist

--- src/containers/Play.js
+++ src/containers/Play.js
@@ -5,8 +5,16 @@ import {useState, useEffect} from 'react'
 
 const Play =() => {
 
-    const [song, setSong] = useState("")
+    const [playlist, setPlaylist] = useState([
+        {
+            title: "SongOne", 
+            songData: ["a", "s", "s", "a"]
+        }
+    ])    
+    const [song, setSong] = useState([])
     const [lastKey, setLastKey] = useState("")
+    const [title, setTitle] = useState("")
+    
     const [keyMap, setKeyMap] = useState({
         "a": {keyPress: "a", note: "C4", color: "red", shape: "circle", beat:"8n"},
         "s": {keyPress: "s", note: "D4", color: "blue", shape: "circle", beat:"8n"}
@@ -14,9 +22,9 @@ const Play =() => {
 
     useEffect(() => {
         document.addEventListener('keydown', ({ key }) => playKey(key))
-        // setLoaded(true)
     }, [])
 
+    // this useEffect saves an array of lastKeys played in the song state.
     useEffect(() => {
         if(lastKey.length === 1){
             setSong([...song, lastKey]);
@@ -30,16 +38,52 @@ const Play =() => {
         const { note, beat } = keyMap[key]
         synth.triggerAttackRelease(note, beat)
         setLastKey(key)
-        setTimeout(() => setLastKey(""), 500)
+        setTimeout(() => setLastKey(""), 1000)
     }
 
+    // const replayKey = playlist[0].songData.forEach((key) => {
+    //    playKey(key)
+    //  }) 
+
+    // const replayPlaylist = () => {
+    //     setInterval(replayKey, 60000)
+    // }
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const titleToSubmit = title.trim()
+        const newSong = {title: titleToSubmit, songData: song}
+        const updatedPlaylist = [...playlist, newSong]
+        setPlaylist(updatedPlaylist)
+        setSong([])
+    }
+
+    // const seq = new Tone.Sequence((time, note) => {
+    //     synth.triggerAttackRelease(note, 0.1, time);
+        
+    //     // subdivisions are given as subarrays
+    // }, ["C4", ["E4", "D4", "E4"], "G4", ["A4", "G4"]]).start(0);
+    // Tone.Transport.start();
 
     return(
         <>
         <h1> This is the Play container</h1>
         <Visual />
         <Instrument pads={keyMap} onKeyClick={playKey} lastKey={lastKey} />
-        <button>Play/Pause</button>
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                placeholder="title" 
+                value={title}
+                onChange={handleTitleChange} 
+            />
+            <input type="submit" value="POST" />
+        </form>
+        <button>Save song</button>
+        <button >Play/Pause</button>
         </>
     )
 }
