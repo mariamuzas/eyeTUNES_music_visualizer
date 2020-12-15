1dcd6f1228df3bb33e33391f74d0cfcc0ba8d7a1
CatAnderson <thecatanderson100@gmail.com>
2020-12-15 12:20:14 +0000

    got animation working with keyPress

--- src/components/KeyVisual.js
+++ src/components/KeyVisual.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 import anime from 'animejs';
-// import { useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 const StyledKeyVisual = styled.div.attrs(props => {
     return {
-        class:`${props.padKey}-visual`,
         style:{
-            animationPlayState: (props.play) ? "running" : "paused",
+            // animationPlayState: (props.play) ? "running" : "paused",
             backgroundColor: props.color
         }
     }
@@ -14,30 +13,42 @@ const StyledKeyVisual = styled.div.attrs(props => {
     position: relative;
     width: 100px;
     height: 100px;
+    border-radius: 100%;
     margin: 4px;
-    display: inline-block;
+    display: block;
 `
 
 
 const KeyVisual = ({color, playState, padKey}) => {
 
-    // const keyRef = useRef(null)
+    const [animation, setAnimation] = useState(null)
 
-    anime({
-        targets: `div.${padKey}-visual`,
-        translateX:[
-            {value: 200, duration: 500},
-            {value: 0, duration: 800}
-        ],
-        rotate: {
-            value: '1turn',
-            easing: 'easeInOutSine'
+    useEffect(()=> {
+        setAnimation(anime({
+            targets: `div.${padKey}-visual`,
+            autoplay: false,
+            translateX:[
+                {value: 200, duration: 375},
+                // {value: 0, duration: 350}
+            ],
+            opacity: [
+                {value: '0%', duration: 0},
+                {value: '100%', duration: 1},
+                {value: '0%', duration: 350}
+            ],
+            easing: 'easeOutSine'
+        }))
+    }, [])
+
+    useEffect(() => {
+        if (!animation) return;
+        if (playState) {
+            animation.play()
         }
-    })
-
+    }, [playState])
 
     return(
-        <StyledKeyVisual color={color} playState={playState} padKey={padKey}/>
+        <StyledKeyVisual color={color} playState={playState} className={`${padKey}-visual`}/>
     )
 }
 
