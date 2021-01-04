e190bfb6b39b2ebdd4ab87e214a149e6ca3de52f
Your Name <mmuzasantuna@gmail.com>
2021-01-04 14:54:39 +0000

    Removed node_module folder

--- client/node_modules/tone/build/esm/core/context/BaseContext.js
+++
@@ -1,8 +0,0 @@
-import { Emitter } from "../util/Emitter";
-export class BaseContext extends Emitter {
-    constructor() {
-        super(...arguments);
-        this.isOffline = false;
-    }
-}
-//# sourceMappingURL=BaseContext.js.map
